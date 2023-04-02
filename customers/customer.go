53557e2ff2c8890dc2199a5428b27fade4b19498
ciel2142 <igi2131@gmail.com>
2023-04-02 21:53:39 +0300

    added favorite order

--- customers/customer.go
+++ customers/customer.go
@@ -1,11 +1,15 @@
 package customers
 
-import "fmt"
+import (
+	"fmt"
+	"modules-go/orders"
+)
 
 type Customer struct {
-	Name    string
-	Surname string
-	Age     int
+	Name          string
+	Surname       string
+	Age           int
+	FavoriteOrder orders.Order
 }
 
 func (customer Customer) FullName() string {
